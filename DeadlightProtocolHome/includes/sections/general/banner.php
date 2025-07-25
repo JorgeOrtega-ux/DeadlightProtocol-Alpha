4bf40f68c9f7c4807fc2bc888abee1498686ad5b
JorgeOrtega-ux <jorge1ortega.484@gmail.com>
2025-07-25 15:57:13 -0500

    Delete DeadlightProtocolHome directory

--- DeadlightProtocolHome/includes/sections/general/banner.php
+++
@@ -1,8 +0,0 @@
-<div class="banner">
-    <div class="banner-title">
-        <h1><span data-translate="banner_title"></span></h1>
-    </div>
-    <div class="banner-subtitle">
-        <h2><span data-translate="banner_subtitle"></span></h2>
-    </div>
-</div>
