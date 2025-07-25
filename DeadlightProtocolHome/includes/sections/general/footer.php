ca950ee9512cac153ff5b85a9a47cadf199c65dd
JorgeOrtega-ux <jorge1ortega.484@gmail.com>
2025-07-25 14:29:29 -0500

    Add files via upload

--- DeadlightProtocolHome/includes/sections/general/footer.php
+++ DeadlightProtocolHome/includes/sections/general/footer.php
@@ -4,21 +4,25 @@
             <span data-translate="copyright"></span>
         </div>
         <div class="footer-item footer-legal body-title">
-            <a data-target-section="privacy-policy"><span data-translate="footer_privacy_policy"></span></a>
-            <a data-target-section="terms-and-conditions"><span data-translate="footer_terms_conditions"></span></a>
-            <a data-target-section="cookie-policy"><span data-translate="footer_cookie_policy"></span></a>
-            <div class="footer-button">
-                <div class="footer-button-icon">
-                    <span class="material-symbols-rounded">language</span>
-                </div>
-                <div class="footer-button-text">
-                    <span data-translate="footer_language_button"></span>
-                </div>
-                <div class="footer-button-icon">
-                    <span class="material-symbols-rounded">arrow_drop_up</span>
-                </div>
+            <div class="footer-legal-links">
+                <a data-target-section="privacy-policy"><span data-translate="footer_privacy_policy"></span></a>
+                <a data-target-section="terms-and-conditions"><span data-translate="footer_terms_conditions"></span></a>
+                <a data-target-section="cookie-policy"><span data-translate="footer_cookie_policy"></span></a>
             </div>
-            <?php include 'includes/modules/module-footer.php'; ?>
+            <div class="footer-language-selector">
+                <div class="footer-button">
+                    <div class="footer-button-icon">
+                        <span class="material-symbols-rounded">language</span>
+                    </div>
+                    <div class="footer-button-text">
+                        <span data-translate="footer_language_button"></span>
+                    </div>
+                    <div class="footer-button-icon">
+                        <span class="material-symbols-rounded">arrow_drop_up</span>
+                    </div>
+                </div>
+                <?php include 'includes/modules/module-footer.php'; ?>
+            </div>
         </div>
     </div>
 </div>
