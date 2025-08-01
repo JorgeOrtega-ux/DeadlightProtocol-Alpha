82664cdd9f42fc8eed2035b454b61c533143528a
JorgeOrtega-ux <jorge1ortega.484@gmail.com>
2025-08-01 14:52:46 -0500

    Add files via upload

--- DeadlightProtocol/index.php
+++ DeadlightProtocol/index.php
@@ -1,11 +1,61 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded">
+    <link rel="stylesheet" type="text/css" href="assets/css/styles.css">
     <title>OperationSentinel - Home</title>
 </head>
+
 <body>
-    
+    <div class="page-wrapper">
+        <div class="main-content">
+            <div class="general-content">
+                <div class="general-content-top">
+                    <div class="header">
+                        <div class="header-left">
+                            <div class="header-item" data-action="toggleModuleSidebar">
+                                <div class="header-button">
+                                    <span class="material-symbols-rounded">menu</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="header-right">
+                            <div class="header-item">
+                                <div class="header-button" data-action="toggleModuleOptions">
+                                    <span class="material-symbols-rounded">settings</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="module-options disabled" data-module="moduleOptions">
+                            <div class="menu-content">
+                                <div class="menu-list">
+                                    <div class="menu-link">
+                                        <div class="menu-link-icon">
+                                            <span class="material-symbols-rounded">settings</span>
+                                        </div>
+                                        <div class="menu-link-text">
+                                            <span>Configuracion</span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="general-content-bottom">
+                    <div class="general-content-scrolleable">
+                        <div class="module-sidebar disabled" data-module="moduleSidebar">
+                            <div class="menu-content"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </body>
+<script src="assets/js/app-init.js"></script>
+
 </html>
